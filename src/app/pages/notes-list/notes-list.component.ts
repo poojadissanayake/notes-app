5c4986ffcfabb1c5191d9166a6a9ba5511dca7c8
Pooja Dissanayake <poojadissanayake6@gmail.com>
2023-07-23 12:20:51 +0530

    Searching notes

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -83,17 +83,75 @@ import { NotesService } from 'src/app/shared/notes.service';
 export class NotesListComponent {
 
   notes: Note[] = new Array<Note>(); 
+  filteredNotes: Note[] = new Array<Note>();
 
   constructor(private notesService: NotesService) {}
 
   ngOnInit() {
     // retrieve all notes from NotesService
     this.notes = this.notesService.getAll();
+    this.filteredNotes = this.notes;
 
   }
 
   deleteNote(id: number) {
     this.notesService.delete(id);
+  }
+
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const value = inputElement.value;
+    this.filter(value);
+  }
+
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+    let allResults: Note[] = new Array<Note>();
+
+    // split up the search query into individual words
+    let terms: string[] = query.split(' '); //split on spaces
+
+    // remove duplicate search terms
+    terms = this.removeDuplicates(terms);
+
+    // compile all relevant results into the allResults array
+    terms.forEach(term => {
+      let results : Note[] = this.relevantNotes(term);
+
+      // append results to the allResults array
+      // array deconstruction in javascript
+      // merge two arrays
+      allResults = [...allResults, ...results]
+    });
+
+    // allResults will include duplicate notes - a particullar note can be the result of many search terms 
+    // but we don't wont to show the same note multiple times on the UI - so 1st we must remove the duplicates
+
+    let uniqueResults = this.removeDuplicates(allResults);
+    this.filteredNotes = uniqueResults;
+  }
+
+  removeDuplicates(arr: Array<any>) : Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+
+    // loop through the input array and add the items to the set
+    arr.forEach(e => uniqueResults.add(e));
+
+    return Array.from(uniqueResults);
+  }
+
+  relevantNotes(query: string): Array<Note> {
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notes.filter(note => {
+      if(note.title && note.title.toLowerCase().includes(query)) {
+        return true;
+      }
+      if(note.body && note.body.toLowerCase().includes(query)) {
+        return true;
+      }
+      return false;
+    })
+    return relevantNotes;
   }
 
 }
